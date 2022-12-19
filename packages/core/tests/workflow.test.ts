f39ac7775f243965024c8a31353924adc29c0848
Pablo Pérez <dalotodo69@gmail.com>
2022-12-19 18:16:52 +0100

    Renamed tests directory

--- packages/core/tests/workflow.test.ts
+++ packages/core/tests/workflow.test.ts
@@ -1,4 +1,4 @@
-import { defineEmits, defineEvent, defineEvents, defineProcess, defineWorkflow } from "../src";
+import { defineEmits, defineEvent, defineEvents, defineProcess, defineWorkflow } from '../src'
 
 test('Basic workflow', () => {
     type Item = { id: string; name: string; }
@@ -10,20 +10,22 @@ test('Basic workflow', () => {
 
     const emit = defineEmits(events);    
 
-    const source = defineProcess(events, {
-        start: (data, ctx)=> {            
-        },        
+    const main = defineProcess(events, (ctx)=> {
+        ctx.on('start', (data)=> {
+            ctx.emit('isValidData', [{id: 'ok', name: 'Is ok'}]);
+        })
+        
     });        
 
     const wf = defineWorkflow({
         events,
         processes: {
-            source
+            main
         }
     })
 
-
     expect(wf.events.start).toBeDefined();
-    expect(wf.processes.source).toBeDefined();
+    expect(wf.processes.main).toBeDefined();
 
 });
+
