1cbd192926812c97b5e85a8a0b4b080f28606df2
Pablo Pérez <pablo.perf@outlook.com>
2022-12-20 23:24:11 +0100

    Additional tests

--- packages/core/tests/execute.test.ts
+++ packages/core/tests/execute.test.ts
@@ -41,11 +41,9 @@ describe('Workflow Execution Tests', () => {
             result: eventOfType<number>(),
         });
 
-
         const wf = defineWorkflow({
             events,
             setup: (ctx, { defineProcess }) => {
-
                 let result = 0;
 
                 const main = defineProcess(() => {
@@ -75,84 +73,6 @@ describe('Workflow Execution Tests', () => {
         })(4).then((value) => {
             expect(value).toEqual(10)
         })
-    })
-
-
-
-    test('Accelerator pedal simulation', () => {
-        const events = defineEvents({
-            // Start engine
-            start: eventOfType<boolean>(),
-
-            // changeSpeed for engine
-            changeSpeed: eventOfType<number>(),
-
-            // displaySpeed in the Odometer
-            displaySpeed: eventOfType<number>(),
-
-            // is Accelerator Pedal pushed (level of hardness)?
-            isAccelPedalPushed: eventOfType<number>(),
-
-            // is Break Pedal pushed (level of hardness)?
-            isBreakPedalPushed: eventOfType<number>(),
-        });
-
-
-        const wf = defineWorkflow({
-            events,
-            setup: (ctx, { defineProcess }) => {
-
-                let speed = 0;
-
-                const brakePedalSensor = defineProcess( () => {
-                    ctx.on('isBreakPedalPushed', (hardness) => {
-                        // Convert hardness from sensor 
-                        if (hardness >= 10) {
-                            return ctx.emit('changeSpeed', -speed);
-                        } else {
-                            return ctx.emit('changeSpeed', -hardness);
-                        }
-                    })
-                })
-
-                const accelPedalSensor = defineProcess(() => {
-                    ctx.on('isAccelPedalPushed', (hardness) => {
-                        ctx.emit('changeSpeed', hardness);
-                    })
-                })
-
-                const main = defineProcess(() => {
-                    ctx.on('start', (isStarted) => {
-                        speed = 0;
-                    })
-
-                    ctx.on('changeSpeed', (increment) => {
-                        speed += increment;
-                        ctx.emit('displaySpeed', speed);          
-                    })
-                })
-
-                return { main, accelPedalSensor, brakePedalSensor };
-            }
-        });
-
-        const drivingCar = buildWorkflow(wf);
-
-        const car = drivingCar();
-
-        let speeds : number[] = [];
-        car.events.displaySpeed.on((newValue) => {
-            speeds.push(newValue);
-        })
-        car.events.start.emit(true);
-
-        car.events.isAccelPedalPushed.emit(1); // Speed = 1
-        car.events.isAccelPedalPushed.emit(2); // Speed = 3 
-        car.events.isAccelPedalPushed.emit(4); // Speed = 7        
-
-        car.events.isBreakPedalPushed.emit(2); // Speed = 5
-        car.events.isBreakPedalPushed.emit(12); // BREAK!! Speed = 0
-        expect(speeds).toEqual([1,3,7,5,0])        
     })
 
 })
