145258e67442a8a7b00edbb33a009c2558e7079c
Pablo Pérez <dalotodo69@gmail.com>
2022-12-20 19:44:09 +0100

    Runtime: Clean-up

--- packages/runtime/tests/runtime.test.ts
+++ packages/runtime/tests/runtime.test.ts
@@ -1,19 +1,4 @@
-import { defineEvent, defineJavascriptProcess, defineWorkflow } from "../src";
 
 test('Basic workflow', () => {
-
-    const wf = defineWorkflow({
-        events: {
-            start: defineEvent<boolean>(),
-        },
-        processes: {
-            source: defineJavascriptProcess((ctx) => {
-            })
-        }
-    })
-
-
-    expect(wf.events.start).toBeDefined();
-    expect(wf.processes.source).toBeDefined();
 
 });
