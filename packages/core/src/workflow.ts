6f48f1ab5d0ebea3a7cdb82739af480a30423623
Pablo Pérez <pablo.perf@outlook.com>
2022-12-21 21:36:31 +0100

    IEvent<T> is callable directly

--- packages/core/src/workflow.ts
+++ packages/core/src/workflow.ts
@@ -1,66 +1,78 @@
-import { Subject, Subscription } from "rxjs"
+import { Subject, Subscription } from "rxjs";
 
-type IEventEmitter<T> = (...data: T[])=>void;
+type IEventEmitter<T> = (...data: T[]) => void;
 type IEventHandlerCallback<T> = (data: T) => void;
-type IEventHandler<T> = (callback: IEventHandlerCallback<T>)=>void;
+type IEventHandler<T> = (callback: IEventHandlerCallback<T>) => void;
 
-interface IEvent<T> {
-    emit: IEventEmitter<T>;
-    on: IEventHandler<T>;
+interface IEvent<T> extends IEventEmitter<T> {
+  emit: IEventEmitter<T>;
+  on: IEventHandler<T>;
 }
 
 export function defineEvent<T>() {
-    const subject = new Subject<T>();
-    const subscriptions: Subscription[] = [];
+  const subject = new Subject<T>();
+  const subscriptions: Subscription[] = [];
 
-    const event: IEvent<T> = {
-        emit: (...data) => (data||[]).forEach( x=>subject.next(x) ),
-        on: (handler: IEventHandlerCallback<T>) => {
-            const subscription = subject.asObservable().subscribe(handler);
-            subscriptions.push(subscription);
-        }
-    }
-    return event;
+  const emitter: IEventEmitter<T> = (...data) =>
+    (data || []).forEach((x) => subject.next(x));
+  const handler: IEventHandler<T> = (callback) => {
+    const subscription = subject.asObservable().subscribe(callback);
+    subscriptions.push(subscription);
+  };
+
+  const event = (() => {
+    const _f: any = (...data: T[]) => emitter(...data);
+    _f.emit = emitter;
+    _f.on = handler;
+    return _f as IEvent<T>;
+  })();
+  return event;
 }
 
 type IProcess<T> = {
-    execute: () => T;
-}
+  execute: () => T;
+};
 
-export function defineProcess<T>( callback: ()=>T ) {
-    const process: IProcess<T> = {
-        execute: callback,
-    }
-    return process;
+export function defineProcess<T>(callback: () => T) {
+  const process: IProcess<T> = {
+    execute: callback,
+  };
+  return process;
 }
 
 type WorkflowDef<E, P> = {
-    events: { [K in keyof E]: IEvent<E[K]>; },
-    processes: { [K in keyof P]: IProcess<P[K]>; },
-}
+  events: { [K in keyof E]: IEvent<E[K]> };
+  processes: { [K in keyof P]: IProcess<P[K]> };
+};
 
-type WorkflowFactory<E,P> = ()=>WorkflowDef<E,P>;
+type WorkflowFactory<E, P> = () => WorkflowDef<E, P>;
 
 type WorkflowExecutionContext = {
-    runtime: { workflowId: string; executionId: string; }
-}
+  runtime: { workflowId: string; executionId: string };
+};
 
-export function defineWorkflow<E, P>(setup: (context: WorkflowExecutionContext) => WorkflowDef<E, P>): WorkflowFactory<E,P> {
-    const workflowId : string = 'wf';
-    let executions : number = 0;
+export function defineWorkflow<E, P>(
+  setup: (context: WorkflowExecutionContext) => WorkflowDef<E, P>
+): WorkflowFactory<E, P> {
+  const workflowId: string = "wf";
+  let executions: number = 0;
 
-    const factory : WorkflowFactory<E,P> = () => {        
-        const executionId : string = `exec-${++executions}`;
+  const factory: WorkflowFactory<E, P> = () => {
+    const executionId: string = `exec-${++executions}`;
 
-        const context : WorkflowExecutionContext = { runtime: { workflowId, executionId } }
+    const context: WorkflowExecutionContext = {
+      runtime: { workflowId, executionId },
+    };
 
-        const workflow: WorkflowDef<E, P> = setup(context);
+    const workflow: WorkflowDef<E, P> = setup(context);
 
-        // Init processes
-        Object.keys(workflow.processes).map(k=>k as keyof P).forEach( k=> {
-            workflow.processes[k].execute();
-        })
-        return workflow;
-    }
-    return factory;
+    // Init processes
+    Object.keys(workflow.processes)
+      .map((k) => k as keyof P)
+      .forEach((k) => {
+        workflow.processes[k].execute();
+      });
+    return workflow;
+  };
+  return factory;
 }
